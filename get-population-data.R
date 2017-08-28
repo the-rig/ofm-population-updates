d2f9e84812d85647ed8f4e3f47fd9c6106dadf5f
NETID\oieeri26 <oieeri26@uw.edu>
2017-08-28 16:05:34 -0700

    changed files names in script

--- get-population-data.R
+++ get-population-data.R
@@ -17,13 +17,13 @@ web_address <- 'http://www.ofm.wa.gov/pop/asr/sade/'
 
 # County:
 
-pop_00_10 <- paste0(web_address, 'ofm_pop_sade_county_2000_to_2010.xlsx')
-pop_10_16 <- paste0(web_address, 'ofm_pop_sade_county_2010_to_2016.xlsx')
+pop_2000_2010 <- paste0(web_address, 'ofm_pop_sade_county_2000_to_2010.xlsx')
+pop_2010_2016 <- paste0(web_address, 'ofm_pop_sade_county_2010_to_2016.xlsx')
 
 # County (special age groups):
 
-pop_s_00_10 <- paste0(web_address, 'ofm_pop_sade_county_2000_to_2010_s.xlsx')
-pop_s_10_16 <- paste0(web_address, 'ofm_pop_sade_county_2010_to_2016_s.xlsx')
+pop_2000_2010_s <- paste0(web_address, 'ofm_pop_sade_county_2000_to_2010_s.xlsx')
+pop_s_2010_2016 <- paste0(web_address, 'ofm_pop_sade_county_2010_to_2016_s.xlsx')
 
 # creating directory for population estimates
 
@@ -33,13 +33,13 @@ dir.create(dir_name)
 
 # downloading County files:
 
-download.file(url = pop_00_10, destfile = paste0(dir_name, '/pop_00_10.xlsx'), mode = 'wb')
-download.file(url = pop_10_16, destfile = paste0(dir_name, '/pop_10_16.xlsx'), mode = 'wb')
+download.file(url = pop_00_10, destfile = paste0(dir_name, '/pop_2000_2010.xlsx'), mode = 'wb')
+download.file(url = pop_10_16, destfile = paste0(dir_name, '/pop_2010_2016.xlsx'), mode = 'wb')
 
 # downloading County (special age groups) files:
 
-download.file(url = pop_s_00_10, destfile = paste0(dir_name, '/pop_s_00_10.xlsx'), mode = 'wb')
-download.file(url = pop_s_10_16, destfile = paste0(dir_name, '/pop_s_10_16.xlsx'), mode = 'wb')
+download.file(url = pop_2000_2010, destfile = paste0(dir_name, '/pop_2000_2010_s.xlsx'), mode = 'wb')
+download.file(url = pop_2010_2016, destfile = paste0(dir_name, '/pop_2010_2016_s.xlsx'), mode = 'wb')
 
 
 
